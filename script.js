c5c3b81d73da9b7227b6f89abd83fb6fabd82bd2
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-12-08 17:31:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+// Write your script here
+
+let counter = 1;
+function increment() {
+  const p = document.querySelector("#counter");
+  alert(p.innerText);
+  p.innerText = counter;
+  counter++;
+}
